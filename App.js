620f5ffc6b7f14916cb2b433cf2dc9e8b9ea0679
OsMax <www.osmax@gmail.com>
2024-03-18 13:25:20 +0200

    copy to new repo

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>I am durbecelo!!!</Text>
+      <Text>VERY BIG DURBECELO!!!!!</Text>
+      <Text>But i'm do it....</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
