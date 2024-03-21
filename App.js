049b419a59698343273a44522baf40bc0eb64e69
OsMax <www.osmax@gmail.com>
2024-03-21 21:13:43 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,19 +1,15 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, SafeAreaView, Image } from "react-native";
+import { StyleSheet } from "react-native";
 import { useFonts } from "expo-font";
-import { Home } from "./src/screens/Home/Home";
 import { RegistrationScreen } from "./src/screens/RegistrationScreen/RegistrationScreen";
 import { LoginScreen } from "./src/screens/LoginScreen/LoginScreen";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { View } from "react-native";
 
 const Stack = createStackNavigator();
-// import { useState } from "react";
 
 export default function App() {
-  // const [screen, setScreen] = useState("");
   const [fontsLoaded] = useFonts({
     "Roboto-Black": require("./assets/fonts/Roboto/Roboto-Black.ttf"),
   });
@@ -21,7 +17,6 @@ export default function App() {
     return null;
   }
   const changeScreen = (screen) => {
-    // console.log(screen);
     setScreen(screen);
   };
 
