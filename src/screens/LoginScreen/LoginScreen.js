3ba8ad586e95e3f72b8b0c783fa7b79f0634b04e
OsMax <www.osmax@gmail.com>
2024-03-21 23:13:22 +0200

    add show password to loginScreen

--- src/screens/LoginScreen/LoginScreen.js
+++ src/screens/LoginScreen/LoginScreen.js
@@ -14,6 +14,7 @@ import { useState } from "react";
 export const LoginScreen = ({ navigation }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [showPass, setShowPass] = useState(true);
 
   return (
     <ImageBackground
@@ -38,12 +39,21 @@ export const LoginScreen = ({ navigation }) => {
               placeholder="Адреса електронної пошти"
               onChangeText={setEmail}
             />
-            <TextInput
-              secureTextEntry={true}
-              style={styles.input}
-              placeholder="Пароль"
-              onChangeText={setPassword}
-            />
+            <View style={styles.passContainer}>
+              <TextInput
+                secureTextEntry={showPass}
+                style={styles.input}
+                placeholder="Пароль"
+                onChangeText={setPassword}
+              />
+              <Pressable
+                style={styles.passBtn}
+                onPressIn={() => setShowPass(false)}
+                onPressOut={() => setShowPass(true)}
+              >
+                <Text>Показати</Text>
+              </Pressable>
+            </View>
             <Pressable
               style={styles.singUpBtn}
               onPress={() => console.log(login, email, password)}
